721b79148bd4f541f0f682455f9b4021da8fa41f
azman0101 <azman0101@gmail.com>
2025-11-05 20:35:11 +0100

    test

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eu
+
+IMAGE="${1:-docker.io/azman0101/aws-imds-packet-analyzer:latest}"
+
+echo "Building image: $IMAGE"
+docker build -t "$IMAGE" .
+
+echo "Built $IMAGE"
+
+echo "To push: docker push $IMAGE"
+
+echo "To deploy the DaemonSet: kubectl apply -f daemonset.yaml"
